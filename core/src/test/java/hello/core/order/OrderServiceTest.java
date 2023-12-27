9f273d71ade10d34f6a4540087aa3ca32bc6bfce
gimeast94 <dudu.gimeast@gmail.com>
2023-12-27 12:48:47 +0900

    주문과 할인 도메인 실행과 테스트

---
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,29 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() throws Exception {
+        //given
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "맥북 M3 프로", 3600000);
+
+        //when
+
+        //then
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+
+    }
+}
