6076f6afdca2373b3c32b204bd0b222794ddbb1f
gimeast94 <dudu.gimeast@gmail.com>
2024-01-04 22:48:19 +0900

    중복 등록과 충돌 - 현재 AutoAppConfig의 ComponentScan에 예외 필터를 걸었는데 제대로 작동하지 않고있음

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,13 +1,20 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) //예제코드 떄문에 필터 설정하였음..
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) //예제코드 때문에 필터 설정하였음..
 )
 public class AutoAppConfig {
 
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
