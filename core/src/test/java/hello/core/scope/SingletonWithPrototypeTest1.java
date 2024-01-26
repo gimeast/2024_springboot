0e1da4a0ef1311fd6c1f5bf412646b7397dc2d52
gimeast <dudu.gimeast@gmail.com>
2024-01-27 01:24:08 +0900

    프로토타입 스코프 - 싱글톤 빈과 함께 사용시 Provider로 문제 해결

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,9 +2,9 @@ package hello.core.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -52,29 +52,42 @@ public class SingletonWithPrototypeTest1 {
 
         System.out.println("count1:" + count1);
         System.out.println("count2:" + count2);
+        prototypeBean.addCount();
         System.out.println("prototypeBean.count:" + prototypeBean.getCount());
 
-        assertThat(count1).isLessThan(count2);
-        assertThat(prototypeBean.getCount()).isNotEqualTo(count2);
+//        assertThat(count1).isLessThan(count2);
+//        assertThat(prototypeBean.getCount()).isNotEqualTo(count2);
+        assertThat(count1).isEqualTo(1);
+        assertThat(count2).isEqualTo(1);
+        assertThat(prototypeBean.getCount()).isEqualTo(1);
 
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
 
+        private final Provider<PrototypeBean> prototypeBeanProvider;
+
+//        private final ObjectProvider<PrototypeBean> prototypeBeanObjectProvider;
+
+//        private final PrototypeBean prototypeBean;
 //        private final ApplicationContext applicationContext;
 
 //        public ClientBean(ApplicationContext applicationContext) {
 //            this.applicationContext = applicationContext;
 //        }
 
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
+        public ClientBean(/*PrototypeBean prototypeBean, *//*ObjectProvider<PrototypeBean> prototypeBeanObjectProvider*/Provider<PrototypeBean> prototypeBeanProvider) {
+//            this.prototypeBeanObjectProvider = prototypeBeanObjectProvider;
+//            this.prototypeBean = prototypeBean;
+            this.prototypeBeanProvider = prototypeBeanProvider;
         }
 
         public int logic() {
 //            PrototypeBean prototypeBean = applicationContext.getBean(PrototypeBean.class);
+//            prototypeBean.addCount();
+//            PrototypeBean prototypeBean = prototypeBeanObjectProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
